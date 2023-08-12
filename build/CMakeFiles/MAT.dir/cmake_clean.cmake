de773b9d1cac1da2aef9d38d71840d4e8d95be41
Decade-qiu <3237550600@qq.com>
2023-08-12 16:56:48 +0800

    build target

---
+++ build/CMakeFiles/MAT.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MAT.dir/utils/murmurhash.cpp.o"
+  "CMakeFiles/MAT.dir/src/MAT.cpp.o"
+  "CMakeFiles/MAT.dir/main.cpp.o"
+  "MAT.pdb"
+  "MAT"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/MAT.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/mat.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/mat.dir/utils/murmurhash.cpp.o"
+  "CMakeFiles/mat.dir/src/MAT.cpp.o"
+  "CMakeFiles/mat.dir/main.cpp.o"
+  "mat.pdb"
+  "mat"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/mat.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
