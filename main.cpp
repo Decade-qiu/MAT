ed73f97602c6174d56411c3ab97d76c1921fe04a
Decade-qiu <3237550600@qq.com>
2023-08-31 20:19:26 +0800

    simplify the main function

--- main.cpp
+++ main.cpp
@@ -1,14 +1,15 @@
 #include "./src/MAT.h"
 #include <iostream>
 
-int main(int argc, char* argv[]){
+int main(){
     // if (argc != 3) {
     //     printf("Usage: %s rule_file_path packet_file_path", argv[0]);
     //     return 1;
     // }
     std::string x = "../data/rules_", x1 = "../data/rules.bak";
     std::string y = "../data/packets_", y1 = "../data/packets.bak"; 
-    for (int i = 1;i <= 10;i++){
+    for (int i = 1;i <= 11;i++){
+        if (i == 11) i = 15;
         printf("====================Test %dk====================\n", i);
 
         read_data_set(x+std::to_string(i)+"k", y+std::to_string(i)+"k");
@@ -24,4 +25,3 @@ int main(int argc, char* argv[]){
         delete_MAT();
     }
 }
-
