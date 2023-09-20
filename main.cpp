10b9b202c7e29c24f75715425939d99fc8b445e9
Decade-qiu <3237550600@qq.com>
2023-09-20 14:08:14 +0800

    main func

--- main.cpp
+++ main.cpp
@@ -7,9 +7,9 @@ int main(){
     // }
     std::string x = "../data/rules_", x1 = "../data/rules.bak";
     std::string y = "../data/packets_", y1 = "../data/packets.bak"; 
-    int a[5] = {0, 1, 8, 15, 22};
-    for (int i = 1;i <= 11;i++){
-        int scale = i;
+    int a[5] = {0, 1, 10, 15, 22};
+    for (int i = 1;i <= 4;i++){
+        int scale = a[i];
         if (i == 11) scale = 15;
         if (i == 12) scale = 22;
         // if (i == 13) scale = 55;
